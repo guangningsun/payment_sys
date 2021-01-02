e4d64e25959469494fa5e60d85dc8d0640f22fd0
sunguangning <sunguangning@126.com>
2021-01-02 19:17:02 +0800

    修改setting bug

--- server/paymentsysapp/settings.py
+++ server/paymentsysapp/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'epidemicapp',
+    'paymentsyscapp',
     'AppModel',
     'corsheaders',
     'mptt',
